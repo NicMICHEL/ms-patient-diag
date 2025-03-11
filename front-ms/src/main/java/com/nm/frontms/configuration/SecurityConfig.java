db97c36122e7122acd436550c8e45519abdc2263
NicMICHEL <nic.chel@gmail.com>
2025-03-11 12:20:53 +0100

    "create front service"

---
+++ front-ms/src/main/java/com/nm/frontms/configuration/SecurityConfig.java
@@ -0,0 +1,42 @@
+package com.nm.frontms.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Value("${clientMSPort}")
+    private String clientMSPort;
+
+    @Bean
+    SecurityFilterChain clientSecurityFilterChain(
+            HttpSecurity http,
+            ClientRegistrationRepository clientRegistrationRepository) throws Exception {
+        http.oauth2Login(Customizer.withDefaults());
+        http.logout((logout) -> {
+            var logoutSuccessHandler =
+                    new OidcClientInitiatedLogoutSuccessHandler(clientRegistrationRepository);
+            logoutSuccessHandler.setPostLogoutRedirectUri("http://localhost:" + clientMSPort + "/client/");
+            logout.logoutSuccessHandler(logoutSuccessHandler);
+        });
+        http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .oauth2Login(withDefaults())
+                .oauth2ResourceServer((oauth2)
+                        -> oauth2.jwt(Customizer.withDefaults()));
+        http.csrf(AbstractHttpConfigurer::disable);
+        return http.build();
+    }
+
+}
